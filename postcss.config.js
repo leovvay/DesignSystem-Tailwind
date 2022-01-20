a08a7da528c4cf9169a2a5eea2424008438e6f74
leovvay <leovvay@gmail.com>
2022-01-20 13:02:13 +0100

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     parser: 'postcss-scss',
     plugins: [
         require('postcss-import'),
-        require('tailwindcss/nesting'),
+        require('tailwindcss/nesting')(require('postcss-nesting')),
         require('postcss-extend'),
         require('tailwindcss'),
         require('autoprefixer'),
